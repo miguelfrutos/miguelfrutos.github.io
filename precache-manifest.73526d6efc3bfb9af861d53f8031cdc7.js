054b8a3ecacbf8a4e4a17cb5036829f1aff12764
miguelfrutos <miguel.frutos@hotmail.com>
2022-06-27 18:23:54 +0200

    Updates

---
+++ precache-manifest.73526d6efc3bfb9af861d53f8031cdc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376b482d561241061da8cd30a9e8a6a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0369100a3bef0818cb25",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "58e100027dae323abdcd",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "0369100a3bef0818cb25",
+    "url": "/static/js/2.78e43ffc.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.78e43ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58e100027dae323abdcd",
+    "url": "/static/js/main.754d8f5e.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
